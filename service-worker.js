083b604cd665ea39b6f4c73c65a4ffcfb848429d
dielzinho25 <diel_zi_nho35@hotmail.com>
2026-06-29 11:31:40 -0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME="batepapo-cache-v508";
-const FILES=["index.html?v=500","style.css?v=500","app.js?v=500","manifest.json?v=500","icon-192.png","icon-512.png"];
-self.addEventListener("install",e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES).catch(()=>{})))});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim()});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))})
